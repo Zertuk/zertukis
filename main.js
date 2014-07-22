cccfebbabca07950bd45ecc408763c220f24e70c
Justin <zertukis@gmail.com>
2014-07-21 23:39:43 -0500

    just messing with some style ideas for the project section

--- main.js
+++ main.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
 		console.log('success');
 		$('html, body').animate({ scrollTop: $(document).height()}, 1000);
 	});	
+
+    $("#right").click(function() {
+    	$('#space_lich_omega').fadeOut('slow');
+        $('#imgur').fadeIn('slow');
+    });
 });
+
+
