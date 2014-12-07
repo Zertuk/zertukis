9d3d50c72b303e5904cc04e8e8c1131c0bd43191
Justin <zertukis@gmail.com>
2014-12-07 00:51:57 -0600

    yeoman angular scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zertukisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zertukisApp
+ */
+angular.module('zertukisApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
