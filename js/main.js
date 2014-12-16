9af692605d0271d00e5dafa14a19cb478333eba9
Justin <zertukis@gmail.com>
2014-12-16 03:43:52 -0600

    added image gallery

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ $(document).ready(function() {
     	}, 1000);
 	});	
 });
+
+
+//image gallery
+
+var images = new Array();
+var gallery;
+
+images[3] = "img/spacelichone.png";
+images[4] = "img/spacelichtwo.png";
+images[5] = "img/spacelichthree.png";
+
+$(document).ready(function() {
+	$('.galleryChoice').click(function() {
+		var galleryIndex = $(this).attr('value');
+		$('.galleryChoice').css('background-color', 'white');
+		$('.galleryChoice').css('color', 'black');
+		$(this).css('background-color', '#ED872D');
+		$(this).css('color', 'white');
+
+		gallery = $(this).parent().prev().find('img.gallery');
+		function imageSwitch(index) {
+			$(gallery).attr("src", images[index]);
+		}
+		imageSwitch(galleryIndex);
+	})
+});
