87314b0d21ca0bd52f63750441b773c8a6d95dcb
Justin <zertukis@gmail.com>
2014-12-23 19:03:29 -0600

    image gallery/lightbox now use small/large images respectively

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
 var images = new Array();
 var gallery;
 
-images[0] = "img/leaguestatsone.png";
-images[1] = "img/leaguestatstwo.png";
-images[3] = "img/spacelichone.png";
-images[4] = "img/spacelichtwo.png";
-images[5] = "img/spacelichthree.png";
-images[6] = "img/imgurgalleryone.png";
-images[7] = "img/imgurgallerytwo.png";
+images[0] = "img/leaguestatsone";
+images[1] = "img/leaguestatstwo";
+images[3] = "img/spacelichone";
+images[4] = "img/spacelichtwo";
+images[5] = "img/spacelichthree";
+images[6] = "img/imgurgalleryone";
+images[7] = "img/imgurgallerytwo";
 
 $(document).ready(function() {
 	$('.galleryChoice').click(function() {
@@ -31,8 +31,8 @@ $(document).ready(function() {
 
 		gallery = $(this).parent().prev().find('img.gallery');
 		function imageSwitch(index) {
-			$(gallery).attr('src', images[index]);
-			$(gallery).attr('value', images[index]);
+			$(gallery).attr('src', images[index] + 'small.png');
+			$(gallery).attr('value', images[index] + '.png');
 			$(gallery).hide();
 			$(gallery).fadeIn();
 		}
